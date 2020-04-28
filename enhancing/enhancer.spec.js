6a0e2d4d18b01b144c9990f259b85e220ffa097b
Josh schaben <jschaben42@gmail.com>
2020-04-27 21:37:11 -0500

    done

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,51 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('the enhancer', () => {
+    
+    describe('the succeed function', () => {
+
+        it('should enhance the item by 10', () => {
+            const item = {
+                name: 'sword',
+                enhancement: 7,
+                level: 3
+            };
+            const success = enhancer.success(item)
+            expect(success.enhancement).toBe(7)
+        })    
+
+        it('should not enhance and return the item', () => {
+            const item = {
+                name: 'sword',
+                enhancement: 7,
+                level: 10
+            };
+            const success = enhancer.success(item)
+            expect(success.enhancement).toBe(17)
+        })
+    })
+
+    describe('the fail status', () => {
+        it('should lower enhancement level by 1 if not high enough level', () => {
+            const item = {
+                name: 'sword',
+                enhancement: 7,
+                level: 3
+            };
+            const fail = enhancer.fail(item)
+            expect(fail.enhancement).toBe(6)
+        })
+    })
+
+    describe('the repair status', () => {
+        it('should repaire the durabilty of the item', () => {
+            const item = {
+                name: 'sword',
+                durability: 2
+            };
+            const repair = enhancer.repair(item)
+            expect(repair.durability).toBe(100)
+        })
+    })
+})
